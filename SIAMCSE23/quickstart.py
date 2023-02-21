11fc4bd5bf850a66a5eb49cf3110c0a0b3045192
Tyler <tchang@anl.gov>
2023-02-21 17:21:18 -0600

    updated code listings and cse23 slides

--- SIAMCSE23/quickstart.py
+++ SIAMCSE23/quickstart.py
@@ -1,28 +1,23 @@
-# Create MOOP
-my_moop = MOOP(LocalGPS)
+from parmoo import MOOP
+from parmoo.optimizers import LocalGPS as gps
+from parmoo.searches import LatinHypercube as lhs
+from parmoo.surrogates import GaussRBF as rbf
+from parmoo.acquisitions import UniformWeights as wsum
+# Create MOOP object with GPS optimizer
+moop = MOOP(gps)
 # Add a continuous + categorical design variable
-my_moop.addDesign({'name': "x1", 'des_type': "continuous",
-                   'lb': 0.0, 'ub': 1.0})
-my_moop.addDesign({'name': "x2", 'des_type': "categorical", 'levels': 3})
-# Define a simulation function
-def sim_func(x):
-   if x["x2"] == 0:
-      return np.array([(x["x1"] - 0.2) ** 2, (x["x1"] - 0.8) ** 2])
-   else:
-      return np.array([99.9, 99.9])
-# Add the simulation with search and surrogate
-my_moop.addSimulation({'name': "MySim",
-                       'm': 2,
-                       'sim_func': sim_func,
-                       'search': LatinHypercube,
-                       'surrogate': GaussRBF,
-                       'hyperparams': {'search_budget': 20}})
-# Add the 2 objectives
-my_moop.addObjective({'name': "f1", 'obj_func': lambda x, s: s["MySim"][0]})
-my_moop.addObjective({'name': "f2", 'obj_func': lambda x, s: s["MySim"][1]})
-# Add 3 acquisition functions
+moop.addDesign({'name': "x1", 'lb': 0.0, 'ub': 1.0})
+moop.addDesign({'name': "x2", 'des_type': "cat", 'levels': 3})
+# Define and add a simulation function (with surrogates and search)
+def s(x): return [(x["x1"]-.2)**2, (x["x1"]-.8)**2] if x["x2"]==0 else [9,9]
+moop.addSimulation({'name': "sim", 'm': 2, 'sim_func': s,
+                    'search': lhs, 'surrogate': rbf})
+# Add 2 objectives
+moop.addObjective({'name': "f1", 'obj_func': lambda x, s: s["sim"][0]})
+moop.addObjective({'name': "f2", 'obj_func': lambda x, s: s["sim"][1]})
+# Add 3 weighted-sum acquisition functions
 for i in range(3):
-    my_moop.addAcquisition({'acquisition': UniformWeights, 'hyperparams': {}})
+    moop.addAcquisition({'acquisition': wsum})
 # Solve with 5 iterations and fetch numpy struct of solutions
-my_moop.solve(5)
-results = my_moop.getPF()
+moop.solve(5)
+results = moop.getPF()
